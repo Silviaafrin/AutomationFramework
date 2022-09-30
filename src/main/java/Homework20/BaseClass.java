6290e3912d782a18dd6ec7b1fe6e76e7698c87af
silviaafrin <silviafariaqa@gmail.com>
2022-09-30 19:19:15 -0400

    white space remove

--- src/main/java/Homework20/BaseClass.java
+++ src/main/java/Homework20/BaseClass.java
@@ -1,29 +1,26 @@
 package Homework20;
 
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
-
 public class BaseClass {
+	WebDriver driver;
 
-		WebDriver driver;
-		@BeforeTest
-		public void openingURL() {
-			System.setProperty("webdriver.chrome.driver","C:\\Users\\faria\\eclipse-workspace\\gov.potal.cms\\driver\\chromedriver.exe");
-			driver=new ChromeDriver();
-			driver.get("https://dor.georgia.gov/");
-			driver.manage().window().maximize();
-			driver.manage().deleteAllCookies();
-			
-		}
-			@AfterTest
-			public void quit() {
-				driver.quit();
-			}		
+	@BeforeTest
+	public void openingURL() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\faria\\eclipse-workspace\\gov.potal.cms\\driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://dor.georgia.gov/");
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+
+	}
+
+	@AfterTest
+	public void quit() {
+		driver.quit();
+	}
 }
-			
-
